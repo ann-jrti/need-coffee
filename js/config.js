1b859eb333423ad868dddce2c8a9bda02627b1f1
ann <asog.jrti@gmail.com>
2025-10-16 12:26:35 +0200

    adds backend url

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 const API_URL = window.location.hostname === 'localhost' 
   ? 'http://localhost:3000'
-  : 'https://YOUR_BACKEND_URL.onrender.com';
+  : 'https://need-coffee.onrender.com';
 
 window.CONFIG = {
   API_URL
