362477c1923dba1bfc1110b3244a54c4cafd2e03
ann <asog.jrti@gmail.com>
2025-10-16 12:15:58 +0200

    adds netlify functions for backend

---
+++ netlify/functions/config.js
@@ -0,0 +1,19 @@
+exports.handler = async (event, context) => {
+  if (event.httpMethod !== 'GET') {
+    return {
+      statusCode: 405,
+      body: JSON.stringify({ error: 'Method not allowed' })
+    };
+  }
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY
+    })
+  };
+};
